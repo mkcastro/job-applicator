cba023b7ef322e07603a2fdce11a66ed9fc537c9
Mike Castro <mkcastro24@gmail.com>
2022-06-02 19:01:56 +0800

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+# Job Applicator
+
+## Setup
+1. Create keywords.txt file
+2. Add your skils to the keywords.txt file
+3. Create a job.txt file
+4. Paste job description into job.txt file
+5. Update .env file
+6. Run index.ts
+7. Copy generated cover letter from output.txt
+
+## To Do
+1. Accept url as job description input
+2. Get keywords from resume pdf
+3. Improve CLI UX by adding options for:
+    1. recipeint name
+    2. position
+    3. reson
+    4. experience
+    5. resume link
+4. Store generated cover leters to be able to add to CLI options
---
+++ index.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const getKeywords = (): string[] => {
+    const keywords = fs.readFileSync('./keywords.txt', 'utf8');
+
+    return keywords.split('\n').filter(keyword => keyword.length > 0);
+}
+
+const getKeywordsFromJob = (): string[] => {
+    const keywords = getKeywords();
+    const job = fs.readFileSync('./job.txt', 'utf8');
+
+    // convert the job.txt file to an array of words separated by either spaces or newlines
+    const jobWords = job.split(/\s|\n/);
+
+    // filter the jobWords array to only include the words that exist in the keywords array
+    return keywords
+        .filter(keyword => jobWords.includes(keyword))
+        .filter((value, index, self) => self.indexOf(value) === index);
+}
+
+const generateCoverLetter = (): string => {
+    const keywords = getKeywordsFromJob();
+    const keywordsString = [
+        // separate keywords with a comma and a single space
+        keywords.slice(0, -1).join(', '),
+        keywords.slice(-1)[0]
+    ]
+        // on the final item on the keywords array, prepend last item with "and"
+        .join(keywords.length < 2 ? '' : ' and ')
+
+    const recipientName = process.env.RECIPIENT_NAME;
+    const position = process.env.POSITION;
+    const reason = process.env.REASON;
+    const experience = process.env.EXPERIENCE;
+    const resumeLink = process.env.RESUME_LINK;
+    const applicantName = process.env.APPLICANT_NAME;
+
+    const result = `Dear ${recipientName},
+
+I'm writing to inquire about the opening for ${position} ${reason}.
+
+${experience} and would make me a strong candidate for this position. I have strong skills in ${keywordsString}.
+
+The top portion of my attached resume highlights my career profile and 3 significant accomplishments that are also in alignment with this position.
+
+I'd welcome the opportunity to speak with you if you feel I'd be a strong candidate for this or any position in your organization.
+
+${resumeLink}
+
+Sincerely,
+
+${applicantName}`;
+
+    return result;
+}
+
+console.log(generateCoverLetter());
+
+// write to output.txt the generated cover letter
+fs.writeFileSync('./output.txt', generateCoverLetter());
